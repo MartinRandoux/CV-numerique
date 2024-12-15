869faa33d25092fcb9e5d9ca8e07b8f22b642230
Martin Randoux <martin.randoux.etu@univ-lille.fr>
2024-12-15 13:10:27 +0100

    modif competence

--- index.js
+++ index.js
@@ -7,19 +7,4 @@ new Typewriter(textAnim, {
     .start()
 
 
-
-
-//JQuery
-$('.btn-opened').on('click', function () {
-    $('.aff').css('display', 'none');
-    $('.btn-opened').css('display', 'none');
-    $('.cacher').css('display', 'block');
-    $('.cacher').css('transition', 'ease-in-out 1s');
-})
-
-$('.btn-closed').on('click', function () {
-    $('.btn-opened').css('display', 'flex');
-    $('.cacher').css('display', 'none');
-})
-
 window.scrollTo(0, 0);
